6d4eb8113e1e24653870ecae08f09b4ce1204748
CATZ <13862383+GatoAzul-SP@users.noreply.github.com>
2025-03-22 05:20:25 -0400

    Correción de elementos innecesarios y formato

--- src/Artists.jsx
+++ src/Artists.jsx
@@ -1,7 +1,4 @@
-import { BrowserRouter as Router } from "react-router-dom"
 import ArtistGrid from "./components/ArtistGrid"
-import SiteHeader from "./components/SiteHeader"
-import SiteFooter from "./components/SiteFooter"
 
 function Artists() {
   return (
